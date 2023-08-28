db8ad3bc2b0744eaad2edbf694b6e8e753ca44c4
Satish5815 <satish.kushwaha@zopsmart.com>
2023-08-28 22:52:58 +0530

    feat: inial file structire

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createPool({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = connection;
